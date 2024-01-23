742c3ddafd83466af0c85fc2876aca3f352a32a5
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-23 17:55:56 +0800

    given b, it worked using different method

--- clone.py
+++ clone.py
@@ -28,13 +28,18 @@ def product_or_sum_of_two_numbers(num1, num2):
         
 value_of_the_two_number = product_or_sum_of_two_numbers(20, 30)
 
+
+print("\n")
+
+
 #Given B.
 def sum_or_product_of_two_numbers():
     number1 = 40
     number2 = 30
     result = number1 * number2
-    # if result <= 1000:
-    #     print("The product is : " + str(result) + ".")
-    # else:
-    #     print("The sum is: " + str(result) + ".")
+    if result <= 1000:
+        print("The product is " + str(result) + ".")
+    else:
+        print("The sum is " + str(result) + ".")
     
+sum_or_product_of_two_numbers()
