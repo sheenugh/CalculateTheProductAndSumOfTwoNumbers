5ce34fd3dba0910771b22786e4dde7ca88b18b8a
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-23 18:04:04 +0800

    putting pseudo code at least

--- clone.py
+++ clone.py
@@ -7,6 +7,8 @@
 # ----- FUNCTIONS -----
 # Given A
 print("Direction: Given the number1 = 20, number 3 = 30, display the output if the product and only the product of the two number is less than or equal to 1000. Otherwise, display the sum.")
+
+# - Using def and if-else statement/function to find whether the total of the two number is product or sum, and print the result
 def product_or_sum_of_two_numbers(num1, num2):
     result = num1 * num2
     if result <= 1000:
@@ -25,6 +27,8 @@ print("\n")
 
 #Given B.
 print("Direction: Given the number1 = 40, number 3 = 30, display the output if the product and only the product of the two number is less than or equal to 1000. Otherwise, display the sum.")
+
+# - Using def and if-else statement/function to find whether the total of the two number is product or sum, and print the result
 def sum_or_product_of_two_numbers():
     number1 = 40
     number2 = 30
