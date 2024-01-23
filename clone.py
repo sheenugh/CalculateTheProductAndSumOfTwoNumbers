ea50e7581afcdaf0e6bca23cb5f74b105f946c3b
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-23 17:48:13 +0800

    Done finding the answer in given a.

--- clone.py
+++ clone.py
@@ -3,7 +3,28 @@
 
 
 #--------------------------------------------------------
-# ----- FUNCTIONS -----
-# ----- ACTUAL CODE -----
+# Exercise 1: Calculate the multiplication and sum of two numbers
+# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+# A.
+# Given: 
+# number1 = 20
+# number2 =   30
 
-#Pseudo Code
+# B. 
+# Given:
+# number1 = 40
+# number2 = 30
+
+
+# ----- FUNCTIONS -----
+# Given A
+def product_of_two_number(num1, num2):
+    result = num1 * num2
+    if result <= 1000:
+        print("The product is " + str(result) + ".")
+    else:
+        result = num1 + num2
+        print(result)
+        
+value_of_the_two_number = product_of_two_number(20, 30)
+
