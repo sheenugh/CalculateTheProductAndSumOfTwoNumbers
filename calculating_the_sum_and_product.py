1031169506a482feb96de2f57cbca7fd523c7055
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-23 18:08:49 +0800

    instead of 'the product or sum' 'the result' nalang

--- calculating_the_sum_and_product.py
+++ calculating_the_sum_and_product.py
@@ -12,10 +12,10 @@ print("Direction: Given the number1 = 20, number 3 = 30, display the output if t
 def product_or_sum_of_two_numbers(num1, num2):
     result = num1 * num2
     if result <= 1000:
-        print("The product is " + str(result) + ".")
+        print("The result is " + str(result) + ".")
     else:
         result = num1 + num2
-        print("The sum is " + str(result) + ".")
+        print("The result is " + str(result) + ".")
         
 value_of_the_two_number = product_or_sum_of_two_numbers(20, 30)
 
@@ -34,8 +34,8 @@ def sum_or_product_of_two_numbers():
     number2 = 30
     result = number1 * number2
     if result <= 1000:
-        print("The product is " + str(result) + ".")
+        print("The result is " + str(result) + ".")
     else:
-        print("The sum is " + str(result) + ".")
+        print("The result is " + str(result) + ".")
     
 sum_or_product_of_two_numbers()
